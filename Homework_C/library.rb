b11755ee4f5c7b88af94fee27a1f4844d78ef3d6
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-05-21 20:24:23 +0100

    Second commit

--- Homework_C/library.rb
+++ Homework_C/library.rb
@@ -1,12 +1,15 @@
 class Library
-  attr_accessor :library
+  # attr_accessor :library
+#Create a class for a Library that has an array of books. Each book should be
+ # a hash with a title, which is a string, and rental details, which is another
+ # hash with a student name and due date.
 
-  def initialize(books)
-    @library = books
+  def initialize(new_books)
+    @books = new_books
   end
 
-  def get_books_library(library)
-    return library[0...3]
+  def get_books
+    return @books
   end
 
 #   def find_book_by_title(library, title)
--- Homework_C/spec/library_spec.rb
+++ Homework_C/spec/library_spec.rb
@@ -42,22 +42,10 @@ def setup
 end
 
 def test_get_books_library
-  library_books = Library.new(@library)
-  get_books_library(@library)
+  library = Library.new(@library[0...3])
+  books = library.get_books
 
-  assert_equal(@library[0...3])
+  assert_equal(@library[0...3], books)
   end
 
-#   def test_pet_shop_name
-#   name = pet_shop_name(@pet_shop)
-#   assert_equal("Camelot of Pets", name)
-# end
-
-#   def test_find_book_by_title
-#     library_books = Library.new(@library)
-#     find_book_by_title(@library, "1984")
-#
-#     assert_equal("1984", library[:title])
-#   end
-#
 end
